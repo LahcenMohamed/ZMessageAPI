689c8c9421587f0a7369cb7b1ccecd174761606b
Mohamed <Mohamed@DESKTOP-T5JL541>
2023-12-03 16:19:21 +0100

    Add project files.

---
+++ Models/Message.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public DateTime Created { get; set; }
+        [ForeignKey("Sender")]
+        public int SenderId { get; set; }
+        [ForeignKey("Recipient")]
+        public int RecipientId { get; set; }
+        public User Sender { get; set; }
+        public User Recipient { get; set; }
+
+    }
+}
